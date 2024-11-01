507ac2f6bc79f3da14e174173a8166c923b832fe
DESKTOP-VA58A1P\Mateusz <ogonekmateusz69@gmail.com>
2024-11-01 20:32:24 +0100

    add Bootstrap to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './global.css'
 import App from './App.jsx'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+import './global.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
